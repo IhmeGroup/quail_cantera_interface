d96c1df913eec4f644f605ea7b353b09ccf9f84f
bbornhoft <bbornhoft@gmail.com>
2022-01-12 16:11:52 -0800

    add reaction rates

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,19 +1,22 @@
-#include "cantera/thermo.h"
+// #include "cantera/thermo.h"
 #include <iostream>
 #include <omp.h>
 #include "thermo.h"
 #include "helpers.h"
+#include <limits>
+
 
 extern "C" {
 
 using namespace Cantera;
+
 
 void get_pressure_interface(const double* Uq, double* P, int ne, int nq, 
     int ns, int nsp, int dim, char* filename)
 {
 
 // Create a new phase
-std::unique_ptr<ThermoPhase> gas(newPhase(filename));
+std::unique_ptr<IdealGasMix> gas(new IdealGasMix(filename));
     for (int ie = 0; ie < ne; ie++){
         for (int iq = 0; iq < nq; iq++){
 
@@ -39,7 +42,7 @@ void get_wavespeed_interface(const double* Uq, double* gamma, double* P, int ne,
 {
 
 // Create a new phase
-std::unique_ptr<ThermoPhase> gas(newPhase(filename));
+std::unique_ptr<IdealGasMix> gas(new IdealGasMix(filename));
 
 // #pragma omp parallel for schedule(static, 1)
     for (int ie = 0; ie < ne; ie++){
@@ -68,7 +71,7 @@ void get_temperature_interface(const double* Uq, double* T, int ne, int nq,
 {
 
 // Create a new phase
-std::unique_ptr<ThermoPhase> gas(newPhase(filename));
+std::unique_ptr<IdealGasMix> gas(new IdealGasMix(filename));
 
 // #pragma omp parallel for schedule(static, 1)
     for (int ie = 0; ie < ne; ie++){
@@ -97,7 +100,7 @@ void get_gamma_interface(const double* Uq, double* gamma, int ne, int nq,
 {
 
 // Create a new phase
-std::unique_ptr<ThermoPhase> gas(newPhase(filename));
+std::unique_ptr<IdealGasMix> gas(new IdealGasMix(filename));
 
 // #pragma omp parallel for schedule(static, 1)
     for (int ie = 0; ie < ne; ie++){
@@ -122,5 +125,43 @@ std::unique_ptr<ThermoPhase> gas(newPhase(filename));
     } // end ne loop
 } // end get_temperature_interface
 
+void get_net_production_rates_interface(const double* Uq, double* S, int ne, int nq, 
+    int ns, int nsp, int dim, char* filename)
+{
+typedef std::numeric_limits< double > dbl;
+std::cout.precision(dbl::max_digits10);
+// Create a new phase
+std::unique_ptr<IdealGasMix> gas(new IdealGasMix(filename));
+double T;
+double Wk;
+// #pragma omp parallel for schedule(static, 1)
+    for (int ie = 0; ie < ne; ie++){
+        for (int iq = 0; iq < nq; iq++){
+            
+            auto start = ie * (nq*ns) + iq * ns;
+            const double *U = Uq + start;
+            const double rho = U[0];
+            const double rhoE = U[dim + 1];
+            double rhoKE = 0;
+            for (int id = 0; id < dim; id++){
+                rhoKE = rhoKE + U[id + 1] * U[id + 1] / rho;
+            }
+            auto e = (rhoE - 0.5 * rhoKE) / rho;
+
+            double Y[nsp];
+            double wdot[nsp];
+            get_massfractions(nsp, rho, U + dim + 2, Y);
+            T = get_T(gas, rho, e, Y);
+
+            gas->getNetProductionRates(wdot);
+
+	       // multiply wdot by molecular weights
+	       for (int is = 0; is < nsp - 1; is++){
+                Wk = gas->molecularWeight(is);
+                S[ie*nq*ns + iq * ns + is + dim + 2] = Wk * wdot[is];
+	       }
+        } // end nq loop
+    } // end ne loop
+} // end get_temperature_interface
 
 } // end extern "C"
